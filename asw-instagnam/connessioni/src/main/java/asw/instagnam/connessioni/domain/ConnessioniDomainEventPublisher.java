1dd35d18bf3c4dc13e66cbea7f6e2ff2bae902da
matteobrandetti <matteo.brandetti@gmail.com>
2020-05-26 17:44:40 +0200

    some fixings

--- asw-instagnam/connessioni/src/main/java/asw/instagnam/connessioni/domain/ConnessioniDomainEventPublisher.java
+++ asw-instagnam/connessioni/src/main/java/asw/instagnam/connessioni/domain/ConnessioniDomainEventPublisher.java
@@ -2,7 +2,6 @@ package asw.instagnam.connessioni.domain;
 
 import asw.instagnam.common.api.event.DomainEvent;
 
-public interface ConnessioniDomainEventPublisher {
-    public void publish(DomainEvent event);
-
+public interface DomainEventPublisher {
+	public void publish(DomainEvent event); 
 }
--- asw-instagnam/connessioni/src/main/java/asw/instagnam/connessioni/domain/DomainEventPublisher.java
+++
@@ -1,7 +0,0 @@
-package asw.instagnam.connessioni.domain;
-
-import asw.instagnam.common.api.event.DomainEvent;
-
-public interface DomainEventPublisher {
-	public void publish(DomainEvent event); 
-}
