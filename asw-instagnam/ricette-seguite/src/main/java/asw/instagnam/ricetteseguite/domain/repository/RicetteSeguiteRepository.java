81d1d55ee22534605451870e17bd3e84b9f1ccb5
matteobrandetti <matteo.brandetti@gmail.com>
2020-06-01 12:35:54 +0200

    Modified Repository

--- asw-instagnam/ricette-seguite/src/main/java/asw/instagnam/ricetteseguite/domain/repository/RicetteSeguiteRepository.java
+++ asw-instagnam/ricette-seguite/src/main/java/asw/instagnam/ricetteseguite/domain/repository/RicetteSeguiteRepository.java
@@ -1,9 +1,14 @@
 package asw.instagnam.ricetteseguite.domain.repository;
 
+import java.util.Collection;
+
 import org.springframework.data.repository.CrudRepository;
 
 import asw.instagnam.ricetteseguite.domain.RicettaSeguita;
 
 public interface RicetteSeguiteRepository extends CrudRepository<RicettaSeguita, Long> {
+	
+	public Collection<RicettaSeguita> findAllByFollower(String follower);
+
 
 }
