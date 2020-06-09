63c559792d813ee65c26fbfe92249c69d099861d
gianluca2097 <gianluca2097@gmail.com>
2020-06-09 14:59:50 +0200

    Modified RicetteSeguiteService

--- asw-instagnam/ricette-seguite/src/main/java/asw/instagnam/ricetteseguite/domain/RicetteSeguiteService.java
+++ asw-instagnam/ricette-seguite/src/main/java/asw/instagnam/ricetteseguite/domain/RicetteSeguiteService.java
@@ -2,8 +2,7 @@ package asw.instagnam.ricetteseguite.domain;
 
 import org.springframework.stereotype.Service;
 
-import asw.instagnam.ricetteseguite.domain.repository.ConnessioniRepository;
-import asw.instagnam.ricetteseguite.domain.repository.RicetteRepository;
+import asw.instagnam.ricetteseguite.domain.repository.RicetteSeguiteRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -14,12 +13,20 @@ import java.util.stream.Collectors;
 public class RicetteSeguiteService {
 
 	@Autowired 
-	private ConnessioniRepository connessioniRepository;
+	private RicetteSeguiteRepository ricetteSeguiteRepository;
 
-	@Autowired 
-	private RicetteRepository ricetteRepository;
+	/*Variante più complessa
+		Trova le ricette create da utenti seguiti da: utente*/
+	public Collection<Ricetta> getRicetteSeguite(String utente) {
+		return ricetteSeguiteRepository.findAllByUtenteFollower(utente).stream()
+				.map(ricettaSeguita -> new Ricetta(ricettaSeguita.getIdRicetta(), ricettaSeguita.getAutoreRicetta(),
+						ricettaSeguita.getTitoloRicetta()))
+				.collect(Collectors.toList());
 
-	/* Trova le ricette (in formato breve) degli utenti seguiti da utente. */
+	}
+	
+	/* Versione base
+	 //Trova le ricette (in formato breve) degli utenti seguiti da utente. 
 	public Collection<Ricetta> getRicetteSeguite(String utente) {
 		//Trova le connessioni in cui utente e' follower
 		return connessioniRepository.findAllByFollower(utente).stream()
@@ -27,6 +34,7 @@ public class RicetteSeguiteService {
 				.map(connessione -> ricetteRepository.findAllByAutore(connessione.getFollowed()))
 				         .flatMap(Collection::stream).collect(Collectors.toList());
 
-	}
+	} 
+	 */
 	
 }
